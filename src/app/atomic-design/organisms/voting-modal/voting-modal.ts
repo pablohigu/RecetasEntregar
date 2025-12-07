55f67fe7319305f7585e50124be02225502bec6b
pablohigu <pablo.hi.do@gmail.com>
2025-12-07 15:32:04 +0100

    feat: implementar VotingModal para valoraciones

--- src/app/atomic-design/organisms/voting-modal/voting-modal.ts
+++ src/app/atomic-design/organisms/voting-modal/voting-modal.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Receta } from '../../recipe.model';
 
 @Component({
-  selector: 'app-voting-modal',
-  imports: [],
+  selector: 'app-ad-voting-modal',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './voting-modal.html',
-  styleUrl: './voting-modal.scss',
+  styleUrl: './voting-modal.scss'
 })
-export class VotingModal {
-
+export class VotingModalComponent {
+  @Input({required: true}) receta!: Receta;
+  @Output() close = new EventEmitter<void>();
+  @Output() confirm = new EventEmitter<number>();
+  
+  hover = 0; 
+  selected = 0;
 }
