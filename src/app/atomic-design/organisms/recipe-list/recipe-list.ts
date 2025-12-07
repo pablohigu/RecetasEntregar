fcacec5b87803baa0f204466fc35e7a9fa35e14a
pablohigu <pablo.hi.do@gmail.com>
2025-12-07 15:37:07 +0100

    feat: actualizar RecipeList para recibir datos via input

--- src/app/atomic-design/organisms/recipe-list/recipe-list.ts
+++ src/app/atomic-design/organisms/recipe-list/recipe-list.ts
@@ -1,22 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RecipeService } from '../../services/recipe.service';
 import { AdRecipeCardComponent } from '../../molecules/recipe-card/recipe-card';
+import { Receta } from '../../recipe.model';
 
 @Component({
   selector: 'app-ad-recipe-list',
+  standalone: true,
   imports: [CommonModule, AdRecipeCardComponent],
   templateUrl: './recipe-list.html',
+  styleUrl: './recipe-list.scss'
 })
 export class AdRecipeListComponent {
-  private recipeService = inject(RecipeService);
-  // Obtenemos el signal de recetas del servicio
-  recipes = this.recipeService.recipes;
-
-  // Método para manejar el evento de borrado
-  handleDelete(id: number) {
-    if (confirm('¿Estás seguro de que quieres borrar esta receta?')) {
-      this.recipeService.deleteRecipe(id);
-    }
-  }
+  recipes = input.required<Receta[]>();
+  deleteRequest = output<string>();
+  voteRequest = output<Receta>();
 }
